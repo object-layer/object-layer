a75bb1c6e472fe09a19d275f711faa3aa26c6ce2
Manuel Vila <mvila@3base.com>
2016-01-26 18:40:16 +0900

    Start RemoteStore implementation

--- src/store/remote.js
+++ src/store/remote.js
@@ -1,9 +1,10 @@
 'use strict';
 
+import fetch from 'isomorphic-fetch';
 import Store from './';
 
 export class RemoteStore extends Store {
-
+  
 }
 
 export default RemoteStore;
---
+++ test/model.js
@@ -0,0 +1,55 @@
+'use strict';
+
+import { assert } from 'chai';
+import { Model, primaryKey, foreignKey, field, createdOn, updatedOn, hasMany } from '../src';
+
+describe('Model', function() {
+  it('should provide decorators to easily define fields', function() {
+    class Person extends Model {
+      @primaryKey() id;
+      @foreignKey() groupId;
+      @field(String) name;
+      @field(Number) age;
+      @createdOn() createdOn;
+      @updatedOn() updatedOn;
+    }
+
+    let fld;
+
+    fld = Person.prototype.primaryKeyField;
+    assert.equal(fld.name, 'id');
+    assert.strictEqual(fld.type, String);
+
+    fld = Person.prototype.getField('groupId');
+    assert.equal(fld.name, 'groupId');
+    assert.strictEqual(fld.type, String);
+
+    fld = Person.prototype.getField('name');
+    assert.equal(fld.name, 'name');
+    assert.strictEqual(fld.type, String);
+
+    fld = Person.prototype.getField('age');
+    assert.equal(fld.name, 'age');
+    assert.strictEqual(fld.type, Number);
+
+    fld = Person.prototype.getField('createdOn');
+    assert.equal(fld.name, 'createdOn');
+    assert.strictEqual(fld.type, Date);
+
+    fld = Person.prototype.getField('updatedOn');
+    assert.equal(fld.name, 'updatedOn');
+    assert.strictEqual(fld.type, Date);
+  });
+
+  it('should provide decorators to define relations', function() {
+    class Person extends Model {
+      @hasMany('Photo', 'personId') photos;
+    }
+
+    let relation = Person.prototype.getRelation('photos');
+    assert.equal(relation.name, 'photos');
+    assert.equal(relation.className, 'Photo');
+    assert.equal(relation.foreignKey, 'personId');
+    assert.equal(relation.type, 'HAS_MANY');
+  });
+}); // Model
